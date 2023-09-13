f857ffc5bd8458bc4bab56ea6179ce7360aaa206
christosmitr <xristosmitragas@gmail.com>
2023-09-13 18:33:54 +0300

    Commit with Git

---
+++ ImageResizeFunction/Services/ImageResizer.cs
@@ -0,0 +1,21 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using System.IO;
+
+
+namespace ImageResizeFunction.Services
+{
+    public class ImageResizer : IImageResizer
+    {
+        public void Resize(Stream input, Stream output)
+        {
+            using (Image image = Image.Load(input))
+            {
+                //Resize height and width of the image by 2
+                image.Mutate(x => x.Resize(image.Width / 2, image.Height / 2));
+                image.Save(output, new JpegEncoder());
+            }
+        }
+    }
+}
